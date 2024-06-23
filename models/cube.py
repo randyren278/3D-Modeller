5cca1f5a21b9170a2acd4caf44f6a359869745a4
randyren278 <randyren278@gmail.com>
2024-06-23 13:24:05 -0600

    fixed sphere, cube variable definition

--- models/cube.py
+++ models/cube.py
@@ -3,4 +3,43 @@ from models.primitive import Primitive
 class Cube(Primitive):
     def __init__(self):
         super(Cube, self).__init__()
-        self.call_list = G_OBJ_CUBE
+        self.call_list = self.create_call_list()
+
+    def create_call_list(self):
+        """ Create a display list for the cube """
+        call_list = glGenLists(1)
+        glNewList(call_list, GL_COMPILE)
+        glBegin(GL_QUADS)
+        # Front Face
+        glVertex3f(-1.0, -1.0,  1.0)
+        glVertex3f( 1.0, -1.0,  1.0)
+        glVertex3f( 1.0,  1.0,  1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        # Back Face
+        glVertex3f(-1.0, -1.0, -1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glVertex3f( 1.0,  1.0, -1.0)
+        glVertex3f( 1.0, -1.0, -1.0)
+        # Top Face
+        glVertex3f(-1.0,  1.0, -1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glVertex3f( 1.0,  1.0,  1.0)
+        glVertex3f( 1.0,  1.0, -1.0)
+        # Bottom Face
+        glVertex3f(-1.0, -1.0, -1.0)
+        glVertex3f( 1.0, -1.0, -1.0)
+        glVertex3f( 1.0, -1.0,  1.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        # Right face
+        glVertex3f( 1.0, -1.0, -1.0)
+        glVertex3f( 1.0,  1.0, -1.0)
+        glVertex3f( 1.0,  1.0,  1.0)
+        glVertex3f( 1.0, -1.0,  1.0)
+        # Left Face
+        glVertex3f(-1.0, -1.0, -1.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glEnd()
+        glEndList()
+        return call_list
--- models/sphere.py
+++ models/sphere.py
@@ -1,6 +1,17 @@
 from models.primitive import Primitive
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 class Sphere(Primitive):
     def __init__(self):
         super(Sphere, self).__init__()
-        self.call_list = G_OBJ_SPHERE
+        self.call_list = self.create_call_list()
+
+    def create_call_list(self):
+        """ Create a display list for the sphere """
+        call_list = glGenLists(1)
+        glNewList(call_list, GL_COMPILE)
+        quadric = gluNewQuadric()
+        gluSphere(quadric, 1.0, 32, 32)
+        glEndList()
+        return call_list
